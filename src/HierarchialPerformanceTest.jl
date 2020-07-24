ce6e84296e6483326487afa132d209c1b2b62eb9
Danny Lin <danny@kdrag0n.dev>
2020-07-24 00:22:22 -0700

    Export PerfMatrix argument type

--- src/HierarchialPerformanceTest.jl
+++ src/HierarchialPerformanceTest.jl
@@ -93,6 +93,6 @@ function calcScore(results::PerfMatrix, reference::PerfMatrix, refScore::Int = 1
     round(Int, hptSpeedup(reference, results) * refScore)
 end
 
-export hpt, hptSpeedup, calcScore
+export PerfMatrix, hpt, hptSpeedup, calcScore
 
 end
